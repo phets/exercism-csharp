b0fdf847a6aed7b5eaf0012fa13ef72fddbac282
phets <fabioregis1980@gmail.com>
2023-01-31 12:08:51 +0100

    Completed 2 exercises.

--- exercises/concept/cars-assemble/CarsAssemble.cs
+++ exercises/concept/cars-assemble/CarsAssemble.cs
@@ -4,16 +4,21 @@ static class AssemblyLine
 {
     public static double SuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
+        if (speed == 0) return 0;
+        if (speed >= 1 && speed <= 4) return 1.0;
+        if (speed >= 5 && speed <= 8) return 0.9;
+        if (speed == 9) return 0.8;
+        if (speed == 10) return 0.77;
+        return 0;
     }
-    
+
     public static double ProductionRatePerHour(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        return speed * 221 * SuccessRate(speed);
     }
 
     public static int WorkingItemsPerMinute(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+        return (int)Math.Floor(ProductionRatePerHour(speed) / 60);
     }
 }
--- exercises/concept/phone-number-analysis/PhoneNumberAnalysis.cs
+++ exercises/concept/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -4,11 +4,14 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        string[] parts = phoneNumber.Split('-');
+        bool IsFake = parts[1] == "555";
+        bool IsNewYork = parts[0] == "212";
+        return (IsNewYork, IsFake, parts[2]);
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+        return phoneNumberInfo.IsFake;
     }
 }
