3bcb2c91d5d59f1dd2d4286a47dfae659a0e1d4b
phets <fabioregis1980@gmail.com>
2023-02-01 22:52:07 +0100

    Simplifeied leap exercise.

--- exercises/practice/leap/Leap.cs
+++ exercises/practice/leap/Leap.cs
@@ -2,11 +2,6 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        if (year % 400 == 0) return true;
-        if (year % 100 == 0) return false;
-        if (year % 4 == 0) return true;
-        return false;
-    }
+    public static bool IsLeapYear(int year) =>
+        year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
