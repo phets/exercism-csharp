766008cbced7dda3bdfed8fcc7c821415c188d01
phets <fabioregis1980@gmail.com>
2023-02-01 21:19:14 +0100

    Complete exercise pythagorean triplet.

--- exercises/practice/pythagorean-triplet/PythagoreanTriplet.cs
+++ exercises/practice/pythagorean-triplet/PythagoreanTriplet.cs
@@ -5,6 +5,20 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        throw new NotImplementedException("You need to implement this function.");
+
+        // a < b < c => a < sum/3
+        for (int a=1; a<sum/3; a++)
+        {
+            for (int b=a+1, c=sum-a-b; b<=(sum-a)/2; b++, c--)
+            {
+                if (IsTriplet((a,b,c)))
+                {
+                    yield return (a,b,c);
+                }
+            }
+        }
     }
+
+    private static bool IsTriplet((int a, int b, int c) tri) =>
+        tri.a * tri.a + tri.b * tri.b == tri.c * tri.c;
 }
