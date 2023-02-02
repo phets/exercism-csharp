3438ceb77f4654f7af3384dd55db27b42b678f57
phets <fabioregis1980@gmail.com>
2023-02-02 23:22:44 +0100

    Completed saddle-points exercise.

--- exercises/practice/saddle-points/SaddlePoints.cs
+++ exercises/practice/saddle-points/SaddlePoints.cs
@@ -6,6 +6,38 @@ public static class SaddlePoints
 {
     public static IEnumerable<(int, int)> Calculate(int[,] matrix)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (IsSaddlePoint(i, j, matrix)) yield return (i + 1, j + 1);
+            }
+        }
+    }
+
+    private static bool IsSaddlePoint(int row, int column, int[,] matrix)
+    {
+
+        int num = matrix[row, column];
+        int colLength = matrix.GetLength(0), colMin = Int32.MaxValue;
+        int rowLength = matrix.GetLength(1), rowMax = Int32.MinValue;
+
+        for (int i = 0; i < colLength; i++)
+        {
+            if (matrix[i, column] < colMin)
+            {
+                colMin = matrix[i, column];
+            }
+        }
+        for (int j = 0; j < rowLength; j++)
+        {
+            if (matrix[row, j] > rowMax)
+            {
+                rowMax = matrix[row, j];
+            }
+        }
+
+        if (colMin == num && rowMax == num) return true;
+        return false;
     }
 }
