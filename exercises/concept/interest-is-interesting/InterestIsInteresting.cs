400d77a79211bfe5b77c1d0b8aed3c9839594124
phets <fabioregis1980@gmail.com>
2023-02-03 05:22:01 +0100

    Completed InterestIsInteresting exercise.

--- exercises/concept/interest-is-interesting/InterestIsInteresting.cs
+++ exercises/concept/interest-is-interesting/InterestIsInteresting.cs
@@ -4,21 +4,27 @@ static class SavingsAccount
 {
     public static float InterestRate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
+        if (balance < 0) return 3.213f;
+        if (balance >= 0 && balance < 1000) return 0.5f;
+        if (balance >= 1000 && balance < 5000) return 1.621f;
+        if (balance >= 5000) return 2.475f;
+        return 0.0f;
     }
 
-    public static decimal Interest(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
-    }
+    public static decimal Interest(decimal balance) =>
+        balance * (decimal)InterestRate(balance) * 0.01m;
 
-    public static decimal AnnualBalanceUpdate(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
-    }
+    public static decimal AnnualBalanceUpdate(decimal balance) =>
+        balance + Interest(balance);
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        int years = 0;
+        while (balance < targetBalance)
+        {
+            years++;
+            balance = AnnualBalanceUpdate(balance);
+        }
+        return years;
     }
 }
