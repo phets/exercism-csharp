d89bd2a0152a07b679724b9e921069ddac34fe48
phets <fabioregis1980@gmail.com>
2023-02-03 06:37:07 +0100

    Completed BirdWatcher exercise.

--- exercises/concept/bird-watcher/BirdWatcher.cs
+++ exercises/concept/bird-watcher/BirdWatcher.cs
@@ -3,39 +3,48 @@ using System;
 class BirdCount
 {
     private int[] birdsPerDay;
+    private static readonly int[] birdsPerDayLastWeek = { 0, 2, 5, 3, 7, 8, 4 };
 
     public BirdCount(int[] birdsPerDay)
     {
         this.birdsPerDay = birdsPerDay;
     }
 
-    public static int[] LastWeek()
-    {
-        throw new NotImplementedException("Please implement the (static) BirdCount.LastWeek() method");
-    }
+    public static int[] LastWeek() =>
+        birdsPerDayLastWeek;
 
-    public int Today()
-    {
-        throw new NotImplementedException("Please implement the BirdCount.Today() method");
-    }
+    public int Today() =>
+        birdsPerDay[birdsPerDay.Length -1];
 
-    public void IncrementTodaysCount()
-    {
-        throw new NotImplementedException("Please implement the BirdCount.IncrementTodaysCount() method");
-    }
+    public void IncrementTodaysCount() =>
+        birdsPerDay[birdsPerDay.Length - 1]++;
 
     public bool HasDayWithoutBirds()
     {
-        throw new NotImplementedException("Please implement the BirdCount.HasDayWithoutBirds() method");
+        foreach(int dailyCount in birdsPerDay)
+        {
+            if (dailyCount == 0) return true;
+        }
+        return false;
     }
 
     public int CountForFirstDays(int numberOfDays)
     {
-        throw new NotImplementedException("Please implement the BirdCount.CountForFirstDays() method");
+        int total = 0;
+        for(int day=0; day<numberOfDays; day++)
+        {
+            total += birdsPerDay[day];
+        }
+        return total;
     }
 
     public int BusyDays()
     {
-        throw new NotImplementedException("Please implement the BirdCount.BusyDays() method");
+        int busyDays = 0;
+        foreach (int dailyCount in birdsPerDay)
+        {
+            if (dailyCount >= 5) busyDays++;
+        }
+        return busyDays;
     }
 }
