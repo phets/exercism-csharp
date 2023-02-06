c9c9cb2237539562abd4e8248c573727f074d0ae
phets <fabioregis1980@gmail.com>
2023-02-06 13:34:01 +0100

    Completed squeaky-clean exercise.

--- exercises/concept/squeaky-clean/SqueakyClean.cs
+++ exercises/concept/squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,30 @@
-using System;
+using System.Text;
 
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        StringBuilder sb = new StringBuilder();
+        bool isAfterDash = false;
+        if (identifier.Length == 0) return identifier;
+        foreach (char c in identifier)
+        {
+            sb.Append(c switch {
+                _ when IsLowercaseGreekLetter(c) => default,
+                _ when isAfterDash => char.ToUpperInvariant(c),
+                _ when char.IsWhiteSpace(c) => '_',
+                _ when char.IsControl(c) => "CTRL",
+                _ when char.IsLetter(c) => c,
+                _ => default,
+            });
+            isAfterDash = c.Equals('-');
+        }
+        return sb.ToString();
+    }
+
+    public static bool IsLowercaseGreekLetter(char c)
+    {
+        int codePoint = (int)c;
+        return codePoint >= 945 && codePoint <= 969;
     }
 }
