9d0f4c8cbed0f34cac3e2caefdf7471fd0ec22bc
phets <fabioregis1980@gmail.com>
2023-02-08 09:55:40 +0100

    Completed rotational-cipher exercise.

--- exercises/practice/rotational-cipher/RotationalCipher.cs
+++ exercises/practice/rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,25 @@
-using System;
+using System.Text;
 
 public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        StringBuilder stringBuilder= new StringBuilder();
+        foreach (char c in text)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                stringBuilder.Append((char)('a' + (c - 'a' + shiftKey) % 26));
+            }
+            else if (c >= 'A' && c <= 'Z')
+            {
+                stringBuilder.Append((char)('A' + (c - 'A' + shiftKey) % 26));
+            }
+            else 
+            {
+                stringBuilder.Append(c);
+            }
+        }
+        return stringBuilder.ToString();
     }
 }
