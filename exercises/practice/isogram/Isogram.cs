95043f5db440a68421764b6a337b392c4df169ba
phets <fabioregis1980@gmail.com>
2023-02-09 10:18:37 +0100

    Completed isogram exercise.

--- exercises/practice/isogram/Isogram.cs
+++ exercises/practice/isogram/Isogram.cs
@@ -1,9 +1,22 @@
-using System;
+using System.Collections.Generic;
+using System.Text;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // Remove non-letter characters from string.
+        StringBuilder stringBuilder= new StringBuilder();
+        foreach(char c in word.ToLower())
+        {
+            if(char.IsLetter(c))
+            {
+                stringBuilder.Append(c);
+            }
+        }
+
+        HashSet<char> letterSet = new HashSet<char>(stringBuilder.ToString().ToCharArray());
+
+        return letterSet.Count == stringBuilder.ToString().Length;
     }
 }
